e9bf6105c775943ec2f482c6955cd770a35add4d
Kar-majue <61873626+Kar-majue@users.noreply.github.com>
2023-03-29 16:41:14 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import * as d3 from "d3";
 
 import ResPage from "./components/0.3_resolution/ResPage";
 import IndicatorChart from "./components/0.4_charts/IndicatorChart";
-import PieChart from "./components/0.4_charts/pieChart";
+import PieChart from "./components/0.4_charts/PieChart";
 
-const CHOARD_DATA_PATH = "/data/chord_data_3.csv";
-const PIE_DATA_PATH = "/data/pie_new@4.csv";
+const CHOARD_DATA_PATH = "/data/chord_new.csv";
+const PIE_DATA_PATH = "/data/pie_new.csv";
 
 const App = () => {
-  // const { page } = stateStore;
+  //   const { page } = stateStore;
 
-  //responsive design
+  // responsive design
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
@@ -68,26 +68,6 @@ const App = () => {
     return root;
   }
 
-  const breadcrumbWidth = 150
-  const breadcrumbHeight = 30
-
-  function breadcrumbPoints(d, i) {
-    const tipWidth = 10;
-    const points = [];
-    points.push("0,0");
-    points.push(`${breadcrumbWidth},0`);
-    points.push(`${breadcrumbWidth + tipWidth},${breadcrumbHeight / 2}`);
-    points.push(`${breadcrumbWidth},${breadcrumbHeight}`);
-    points.push(`0,${breadcrumbHeight}`);
-    if (i > 0) {
-      // Leftmost breadcrumb; don't include 6th vertex.
-      points.push(`${tipWidth},${breadcrumbHeight / 2}`);
-    }
-    return points.join(" ");
-  }
-
-  
-
   // handle windows resize
   useEffect(() => {
     function handleResize() {
@@ -116,10 +96,11 @@ const App = () => {
         let names = Array.from(
           new Set(data.flatMap((d) => [d.Stakeholders, d.Target]))
         );
+        let version = `value_${Math.floor(Math.random()*10)}`;
         let index = new Map(names.map((name, i) => [name, i]));
         let matrix = Array.from(index, () => new Array(names.length).fill(0));
-        for (const { Stakeholders, Target, count } of data)
-          matrix[index.get(Stakeholders)][index.get(Target)] += Number(count);
+        for (const { Stakeholders, Target, [version]:value } of data)
+          matrix[index.get(Stakeholders)][index.get(Target)] += Number(value);
 
         console.log(names, matrix, data);
         setChordData({ names: names, matrix: matrix, data: data });
@@ -152,6 +133,64 @@ const App = () => {
 
   }, []);
 
+  // load pie data
+  useEffect(() => {
+    fetch(PIE_DATA_PATH)
+      .then((response) => response.text())
+      .then((csvData) => {
+        let sample = d3.csvParseRows(csvData);
+        delete sample.columns;
+        let data = buildHierarchy(sample)
+        let root = d3.hierarchy(data).sum(d => d.value).sort((a, b) => b.value - a.value);
+        let partition = d3.partition().size([2 * Math.PI, root.height + 1])
+        let version = 1
+
+        function buildHierarchy(csv) {
+          // Helper function that transforms the given CSV into a hierarchical format.
+          const root = { name: "root", children: [] };
+          for (let i = 0; i < csv.length; i++) {
+            const sequence = csv[i][0];
+            const size = +csv[i][version];
+            if (isNaN(size)) {
+              // e.g. if this is a header row
+              continue;
+            }
+            const parts = sequence.split("-");
+            let currentNode = root;
+            for (let j = 0; j < parts.length; j++) {
+              const children = currentNode["children"];
+              const nodeName = parts[j];
+              let childNode = null;
+              if (j + 1 < parts.length) {
+                // Not yet at the end of the sequence; move down the tree.
+                let foundChild = false;
+                for (let k = 0; k < children.length; k++) {
+                  if (children[k]["name"] == nodeName) {
+                    childNode = children[k];
+                    foundChild = true;
+                    break;
+                  }
+                }
+                // If we don't already have a child node for this branch, create it.
+                if (!foundChild) {
+                  childNode = { name: nodeName, children: [] };
+                  children.push(childNode);
+                }
+                currentNode = childNode;
+              } else {
+                // Reached the end of the sequence; create a leaf node.
+                childNode = { name: nodeName, value : size };
+                children.push(childNode);
+              }
+            }
+          }
+          return root;
+        }
+       // process data
+        setPieData({ sample: sample, data: data, partition: partition });
+      });
+  }, []);
+
   return isMobile ? (
     <ResPage />
   ) : (
@@ -167,4 +206,3 @@ export default App;
 export function renderToDOM(container) {
   render(<App />, container);
 }
-
